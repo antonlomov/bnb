7a61060daeed23579925f65fea450b058711cb48
anton lomov <anton.lomov@elcmarketing.com>
2015-06-15 15:53:41 +0200

    schema

--- db/schema.rb
+++ db/schema.rb
@@ -16,12 +16,20 @@ ActiveRecord::Schema.define(version: 20150615130800) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
-
   create_table "appartments", force: :cascade do |t|
     t.string   "address"
     t.string   "property_type"
     t.integer  "nbr_rooms"
     t.integer  "capacity"
+    t.datetime "created_at",    null: false
+    t.datetime "updated_at",    null: false
+  end
+
+  create_table "users", force: :cascade do |t|
+    t.string   "first_name"
+    t.string   "last_name"
+    t.string   "email_address"
+    t.string   "phone_number"
     t.datetime "created_at",    null: false
     t.datetime "updated_at",    null: false
   end
