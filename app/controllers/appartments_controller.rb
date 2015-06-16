0d3b1c56045b65a6c291e551d23ef0192d0c5b1d
Linde Vloeberghs <lindevloeberghs@me.com>
2015-06-16 15:30:57 +0200

    fixed conflict

--- app/controllers/appartments_controller.rb
+++ app/controllers/appartments_controller.rb
@@ -1,4 +1,9 @@
 class AppartmentsController < ApplicationController
+  skip_before_action :authenticate_account!, only: [:index, :show, :home]
+
+  def home
+  end
+
   def show
     @appartment = Appartment.find(params[:id])
 
@@ -14,7 +19,6 @@ class AppartmentsController < ApplicationController
     @property_types = Appartment::PROPERTY_TYPES
     @room_numbers = Appartment::ROOM_NUMBERS
     @capacities = Appartment::CAPACITIES
-
   end
 
   def create
@@ -24,7 +28,6 @@ class AppartmentsController < ApplicationController
     else
       render :new
     end
-
   end
 
   def index
@@ -51,6 +54,8 @@ class AppartmentsController < ApplicationController
     @appartment.destroy
     redirect_to appartments_path
   end
+
+  protected
 
   def appartment_params
     params.require(:appartment).permit(:address, :property_type, :nbr_rooms, :capacity)
---
+++ app/controllers/registrations_controller.rb
@@ -0,0 +1,11 @@
+class RegistrationsController < Devise::RegistrationsController
+  # (**) so here we create a new registrationscontroller, which will inherit from the devise registrationscontroller
+  #      and we only overwrite the piece we want to change i.e. the create accounts' action's redirect_to part
+  #      https://github.com/plataformatec/devise/wiki/How-To:-Redirect-to-a-specific-page-on-successful-sign-up-%28registration%29
+  protected
+
+  def after_sign_up_path_for(resource)
+    # so here we give the path the end of the account's creation action should redirect to instead of the default devise redirect
+    new_user_path
+  end
+end
