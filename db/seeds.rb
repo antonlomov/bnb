2181775bd2b26d7ae511cd0e6da4eea3c1914716
Linde Vloeberghs <lindevloeberghs@me.com>
2015-06-17 12:50:39 +0200

    seeded 5 users and 3 owners with each 2 appartments

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,11 +6,6 @@
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
 
-
-PROPERTY_TYPES = %w(Apartment House Room)
-ROOM_NUMBERS = (1..5).to_a
-CAPACITIES = (1..12).to_a
-
 # creating users (without apps)
 5.times do
   user = User.create(
@@ -18,9 +13,10 @@ CAPACITIES = (1..12).to_a
     last_name: Faker::Name.last_name,
     phone_number: Faker::PhoneNumber.cell_phone,
     picture: 'http://lorempixel.com/30/30/people')
-  Account.create(
-    email: Faker::Internet.email,
-    encrypted_password: Faker::Internet.password(8),
+  password = Faker::Internet.password(8)
+  Account.create(email: Faker::Internet.email,
+    password: password,
+    password_confirmation: password,
     user: user)
 end
 
@@ -30,18 +26,18 @@ end
     first_name: Faker::Name.first_name,
     last_name: Faker::Name.last_name,
     phone_number: Faker::PhoneNumber.cell_phone,
-    )
-  Account.create(
-    email: Faker::Internet.email,
-    encrypted_password: Faker::Internet.password(8),
-    user: owner
-    )
+    picture: 'http://lorempixel.com/30/30/people')
+  password = Faker::Internet.password(8)
+  Account.create(email: Faker::Internet.email,
+    password: password,
+    password_confirmation: password,
+    user: owner)
   2.times do
     Appartment.create(
     address: Faker::Address.street_address + Faker::Address.city,
-    property_type: PROPERTY_TYPES.sample,
-    nbr_rooms: ROOM_NUMBERS.sample,
-    capacity: CAPACITIES.sample,
+    property_type: %w(Apartment House Room).sample,
+    nbr_rooms: (1..5).to_a.sample,
+    capacity: (1..12).to_a.sample,
     owner: owner,
     picture: 'http://lorempixel.com/30/30/city')
   end
