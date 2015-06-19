a8b4f727ea6ab7948f7ce625dc904d2320511f44
Linde Vloeberghs <lindevloeberghs@me.com>
2015-06-19 16:20:50 +0200

    FIXED BUG WITH HERO SIMON

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -13,19 +13,23 @@ class UsersController < ApplicationController
     # @errors = [] # for passing it in the render
   end
 
-  def create
-    @user = User.new(user_params)
-    # link user to account (where we come from) -> comes from the session cookie!!
-    # current_account is sth you can use everywhere withi the session
-     # works because we are somewhere where we need a login (before_action in application controller), so there's always a session linked to an account
-     # with account because we did devise on account
-    @user.account = current_account
-    if @user.save
-      redirect_to user_path(@user)
-    else
-      render :new
-    end
-  end
+  # def create
+  #   # FIXING BUG - replacing by creation of new user with test values
+  #   # @user = User.new(user_params)
+  #   @user = User.new(first_name: "first name", last_name: "last name")
+  #   # link user to account (where we come from) -> comes from the session cookie!!
+  #   # current_account is sth you can use everywhere withi the session
+  #    # works because we are somewhere where we need a login (before_action in application controller), so there's always a session linked to an account
+  #    # with account because we did devise on account
+  #   @user.account = current_account
+  #   if @user.save
+  #     # FIXING BUG - replacing by going to edit user instead of to show user, as test values only now
+  #     # redirect_to user_path(@user)
+  #     redirect_to edit_user_path(@user)
+  #   else
+  #     render :new
+  #   end
+  # end
 
   def edit
     @user = User.find(params[:id])
